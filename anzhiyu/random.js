58c247b4a77c6c000af1f4d483a6ddd5738f56b8
ZhouyiStudio <yao-zhouyi@outlook.com>
2025-12-20 07:30:59 +0800

    Site updated: 2025年12月20日 07:30:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello-world/"];function toRandomPost(){
+var posts=["hello-world/","怎么搭建Hexo博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
