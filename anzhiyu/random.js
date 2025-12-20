da0f3c0b43124eb396e56196607679e1042bc792
ZhouyiStudio <yao-zhouyi@outlook.com>
2025-12-20 12:41:21 +0800

    Site updated: 2025年12月20日 12:41:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello-world/","怎么搭建Hexo博客/"];function toRandomPost(){
+var posts=["hello-world/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
