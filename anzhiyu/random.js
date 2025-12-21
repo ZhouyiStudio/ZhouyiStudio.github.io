95c001d65375ea89fb48b625ee6fb7f2057387a6
ZhouyiStudio <yao-zhouyi@outlook.com>
2025-12-21 13:45:05 +0800

    Site updated: 2025年12月21日 13:45:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["如何快速的、大量的清理Linux系统废物文件/","hello-world/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/"];function toRandomPost(){
+var posts=["hello-world/","如何快速的、大量的清理Linux系统废物文件/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
