d449319d1ae41bfcb36bf46edef8b9edc420705d
ZhouyiStudio <yao-zhouyi@outlook.com>
2026-02-09 13:26:40 +0800

    Site updated: 2026年2月9日 13:26:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Git-Push失败？教你用SSH链接轻松解决/","2b2tCN-Minecraft服务器管理地址/","Git（Windows版）快速安装教程/","Git生成SSHKey教程（Windows版）/","2b2tCN生存服1-21-11版本大更新公告/","hello-world/","如何快速的、大量的清理Linux系统废物文件/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/"];function toRandomPost(){
+var posts=["2b2tCN-Minecraft服务器管理地址/","2b2tCN生存服1-21-11版本大更新公告/","Git-Push失败？教你用SSH链接轻松解决/","Git生成SSHKey教程（Windows版）/","hello-world/","Git（Windows版）快速安装教程/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/","如何快速的、大量的清理Linux系统废物文件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
