007931719c7143b0ed429a42bcda521d6d86c775
ZhouyiStudio <yao-zhouyi@outlook.com>
2026-03-02 20:32:06 +0800

    Site updated: 2026年3月2日 20:32:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["AI角色扮演游戏框架SillyTavern（酒馆）的本地与云服务器搭建/","Git-Push失败？教你用SSH链接轻松解决/","hello-world/","2b2tCN-Minecraft服务器管理地址/","Git（Windows版）快速安装教程/","如何快速的、大量的清理Linux系统废物文件/","如何将hexo个人博客部署到个人云服务器上/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/"];function toRandomPost(){
+var posts=["hello-world/","2b2tCN-Minecraft服务器管理地址/","Git（Windows版）快速安装教程/","Git-Push失败？教你用SSH链接轻松解决/","AI角色扮演游戏框架SillyTavern（酒馆）的本地与云服务器搭建/","如何快速的、大量的清理Linux系统废物文件/","新手零门槛！雨云MC服务器开服全攻略，10分钟搞定专属服务器/","如何将hexo个人博客部署到个人云服务器上/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
